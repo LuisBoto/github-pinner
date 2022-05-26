332e98f71dce2c8c159ebbae26d3304bbd401568
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-05-26 16:43:32 +0200

    Updated css URL

--- GitHubPinner.js
+++ GitHubPinner.js
@@ -87,7 +87,7 @@
     var styleref = document.createElement("link")
     styleref.rel = "stylesheet"
     styleref.type = "text/css"
-    styleref.href = "css/style.css"//"https://d29mk5socxaj4o.cloudfront.net/css/style.css"
+    styleref.href = "https://luisboto.github.io/github-pinner/css/style.css" // "https://d29mk5socxaj4o.cloudfront.net/css/style.css"
     document.getElementsByTagName("head")[0].prepend(styleref)
   }
 
